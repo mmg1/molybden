a9715af08a68c76188bee30d8e3f74ab66bc1257
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-24 18:11:42 +0300

    feat(src): add main file
    feat(src): add main file

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const glob = require('glob').sync
+const path = require('path')
+const serve = require('serve')
+const port = process.env.PORT || 5000
+const testFilesPath = './domato/dist'
+const servePath = path.join(__dirname, '../', testFilesPath)
+// run server
+const server = serve(servePath, {
+	port
+})
+const fuzzFiles = glob(`${testFilesPath}/*.html`).sort((a, b) => a - b)
+;(async () => {
+	// path to Chromium asan build
+	// You can download chromium with asan at:
+	// https://commondatastorage.googleapis.com/chromium-browser-asan/index.html?prefix=mac-release/
+	const executablePath = `../asan-mac-release`
+	const launchOptions = {
+		// headless: false,
+		// slowMo: 250
+		executablePath,
+		timeout: 10000
+	}
+	const browser = await puppeteer.launch(launchOptions)
+	const page = await browser.newPage()
+	for (filename of fuzzFiles) {
+		const startedAt = Date.now()
+		const filenameNoDirPrefix = filename.slice(
+			filename.lastIndexOf('/') + 1,
+			filename.length
+		)
+		const url = `http://127.0.0.1:${port}/${filenameNoDirPrefix}`
+		console.log(url)
+		try {
+			await page.goto(url, {
+				timeout: 6000,
+				waitUntil: 'load'
+			})
+		} catch (e) {
+			console.error(
+				`ITSATRAP! http://localhost:${port}/${filenameNoDirPrefix}`,
+				e
+			)
+		}
+		console.log(`Exec time: ${(Date.now() - startedAt) / 1000}s`)
+	}
+	await browser.close()
+	server.stop()
+})()
